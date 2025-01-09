af7ca61d1d838b3548f834f6320d2e266c47d8c0
weeger <romain.weeger@gmail.com>
2025-01-09 11:26:58 +0100

    Fix append children and test adding a missing line

--- wexample_filestate_python/workdir/python_package_workdir.py
+++ wexample_filestate_python/workdir/python_package_workdir.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, List
 
 from wexample_filestate_python.workdir.python_workdir import PythonWorkdir
 from wexample_config.const.types import DictConfig
+from wexample_helpers.helpers.array import array_dict_get_by
 
 
 class PythonPackageWorkdir(PythonWorkdir):
@@ -10,14 +11,22 @@ class PythonPackageWorkdir(PythonWorkdir):
 
         config = super().prepare_value(config)
 
-        config.update({
-            "children": [
-                {
-                    'name': 'pyproject.toml',
-                    'type': DiskItemType.FILE,
-                    'should_exist': True,
-                },
-            ]
-        })
+        # Retrieve the '.gitignore' configuration or create it if it doesn't exist
+        config_gitignore = array_dict_get_by('name', '.gitignore', config["children"])
+        if config_gitignore is not None:
+            # Use setdefault to initialize 'should_contain_lines' if not already present
+            should_contain_lines = config_gitignore.setdefault("should_contain_lines", [])
+            if isinstance(should_contain_lines, list):
+                should_contain_lines.append("*.egg-info")
+            else:
+                raise ValueError("'should_contain_lines' must be a list")
+
+        config["children"].append(
+            {
+                'name': 'pyproject.toml',
+                'type': DiskItemType.FILE,
+                'should_exist': True,
+            }
+        )
 
         return config
