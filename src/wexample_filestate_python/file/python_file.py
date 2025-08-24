3137ce6be39cdffb9e992c3e9254f83e445d12e0
weeger <romain.weeger@gmail.com>
2025-08-24 20:17:05 +0200

    Build publication dependencies

---
+++ src/wexample_filestate_python/file/python_file.py
@@ -0,0 +1,10 @@
+from typing import ClassVar
+
+from wexample_filestate.item.item_target_file import ItemTargetFile
+
+
+class PythonFile(ItemTargetFile):
+    EXTENSION_ENV: ClassVar[str] = "py"
+
+    def _expected_file_name_extension(self) -> str | None:
+        return PythonFile.EXTENSION_ENV
