b844d9e5c594935e25a53cfc11da65859af9fb61
weeger <romain.weeger@gmail.com>
2025-08-27 14:34:06 +0200

    Publish

--- src/wexample_filestate_python/common/pipy_gateway.py
+++ src/wexample_filestate_python/common/pipy_gateway.py
@@ -6,12 +6,11 @@ from wexample_helpers_api.common.abstract_gateway import AbstractGateway
 
 class PipyGateway(AbstractGateway):
     base_url: str | None = Field(
-        default="https://pypi.org/",
-        description="Base Pipy API URL"
+        default="https://pypi.org/", description="Base Pipy API URL"
     )
 
     def package_release_exists(self, package_name: str, version: str) -> bool:
-        response = self.make_request(f'pypi/{package_name}/json')
+        response = self.make_request(f"pypi/{package_name}/json")
         # Package exists
         if response.status_code == 200:
             return bool(response.json().get("releases", {}).get(version))
