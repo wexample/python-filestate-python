69eb523dd81c251e12e1d3e7318e6d38858d4d4f
weeger <romain.weeger@gmail.com>
2025-09-19 08:42:48 +0200

    Move

--- src/wexample_filestate_python/options_provider/python_options_provider.py
+++ src/wexample_filestate_python/options_provider/python_options_provider.py
@@ -15,10 +15,10 @@ if TYPE_CHECKING:
 class PythonOptionsProvider(AbstractOptionsProvider):
     @classmethod
     def get_options(cls) -> list[type[AbstractConfigOption]]:
-        from wexample_filestate_python.config_option.python_config_option import (
-            PythonConfigOption,
+        from wexample_filestate_python.option.python_option import (
+            PythonOption,
         )
 
         return [
-            PythonConfigOption,
+            PythonOption,
         ]
