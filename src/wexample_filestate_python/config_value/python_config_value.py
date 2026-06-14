933103677b0ed1a4435e9dc03708f19e816f6c88
wexample_pip_suite_manager-performance agent <agent.wexample_pip_suite_manager-performance@wex.local>
2026-06-14 16:33:02 +0200

    [suite:wexample_pip_suite_manager/performance] cache 22 deferred imports and get_name() calls in module-level dict, paying cost only once per process

--- src/wexample_filestate_python/config_value/python_config_value.py
+++ src/wexample_filestate_python/config_value/python_config_value.py
@@ -6,6 +6,105 @@ from wexample_config.config_value.config_value import ConfigValue
 from wexample_helpers.classes.field import public_field
 from wexample_helpers.decorator.base_class import base_class
 
+# Cached mapping: option-name → self attribute name.
+# Built once on first call to to_option_raw_value() so the 22 deferred
+# imports and get_name() class-method calls are paid only once per process.
+_OPTION_NAME_TO_ATTR: dict[str, str] | None = None
+
+
+def _build_option_name_to_attr() -> dict[str, str]:
+    from wexample_filestate_python.config_option.add_future_annotations_config_option import (
+        AddFutureAnnotationsConfigOption,
+    )
+    from wexample_filestate_python.config_option.add_return_types_config_option import (
+        AddReturnTypesConfigOption,
+    )
+    from wexample_filestate_python.config_option.fix_attrs_config_option import (
+        FixAttrsConfigOption,
+    )
+    from wexample_filestate_python.config_option.fix_blank_lines_config_option import (
+        FixBlankLinesConfigOption,
+    )
+    from wexample_filestate_python.config_option.format_config_option import (
+        FormatConfigOption,
+    )
+    from wexample_filestate_python.config_option.fstringify_config_option import (
+        FstringifyConfigOption,
+    )
+    from wexample_filestate_python.config_option.modernize_typing_config_option import (
+        ModernizeTypingConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_class_attributes_config_option import (
+        OrderClassAttributesConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_class_docstring_config_option import (
+        OrderClassDocstringConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_class_methods_config_option import (
+        OrderClassMethodsConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_constants_config_option import (
+        OrderConstantsConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_iterable_items_config_option import (
+        OrderIterableItemsConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_main_guard_config_option import (
+        OrderMainGuardConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_module_docstring_config_option import (
+        OrderModuleDocstringConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_module_functions_config_option import (
+        OrderModuleFunctionsConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_module_metadata_config_option import (
+        OrderModuleMetadataConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_spacing_config_option import (
+        OrderSpacingConfigOption,
+    )
+    from wexample_filestate_python.config_option.order_type_checking_block_config_option import (
+        OrderTypeCheckingBlockConfigOption,
+    )
+    from wexample_filestate_python.config_option.relocate_imports_config_option import (
+        RelocateImportsConfigOption,
+    )
+    from wexample_filestate_python.config_option.remove_unused_config_option import (
+        RemoveUnusedConfigOption,
+    )
+    from wexample_filestate_python.config_option.sort_imports_config_option import (
+        SortImportsConfigOption,
+    )
+    from wexample_filestate_python.config_option.unquote_annotations_config_option import (
+        UnquoteAnnotationsConfigOption,
+    )
+
+    return {
+        AddFutureAnnotationsConfigOption.get_name(): "add_future_annotations",
+        AddReturnTypesConfigOption.get_name(): "add_return_types",
+        FixAttrsConfigOption.get_name(): "fix_attrs",
+        FixBlankLinesConfigOption.get_name(): "fix_blank_lines",
+        FormatConfigOption.get_name(): "format",
+        FstringifyConfigOption.get_name(): "fstringify",
+        ModernizeTypingConfigOption.get_name(): "modernize_typing",
+        OrderClassAttributesConfigOption.get_name(): "order_class_attributes",
+        OrderClassDocstringConfigOption.get_name(): "order_class_docstring",
+        OrderClassMethodsConfigOption.get_name(): "order_class_methods",
+        OrderConstantsConfigOption.get_name(): "order_constants",
+        OrderIterableItemsConfigOption.get_name(): "order_iterable_items",
+        OrderMainGuardConfigOption.get_name(): "order_main_guard",
+        OrderModuleDocstringConfigOption.get_name(): "order_module_docstring",
+        OrderModuleFunctionsConfigOption.get_name(): "order_module_functions",
+        OrderModuleMetadataConfigOption.get_name(): "order_module_metadata",
+        OrderSpacingConfigOption.get_name(): "order_spacing",
+        OrderTypeCheckingBlockConfigOption.get_name(): "order_type_checking_block",
+        RelocateImportsConfigOption.get_name(): "relocate_imports",
+        RemoveUnusedConfigOption.get_name(): "remove_unused",
+        SortImportsConfigOption.get_name(): "sort_imports",
+        UnquoteAnnotationsConfigOption.get_name(): "unquote_annotations",
+    }
+
 
 @base_class
 class PythonConfigValue(ConfigValue):
@@ -102,94 +201,10 @@ class PythonConfigValue(ConfigValue):
     )
 
     def to_option_raw_value(self) -> Any:
-        from wexample_filestate_python.config_option.add_future_annotations_config_option import (
-            AddFutureAnnotationsConfigOption,
-        )
-        from wexample_filestate_python.config_option.add_return_types_config_option import (
-            AddReturnTypesConfigOption,
-        )
-        from wexample_filestate_python.config_option.fix_attrs_config_option import (
-            FixAttrsConfigOption,
-        )
-        from wexample_filestate_python.config_option.fix_blank_lines_config_option import (
-            FixBlankLinesConfigOption,
-        )
-        from wexample_filestate_python.config_option.format_config_option import (
-            FormatConfigOption,
-        )
-        from wexample_filestate_python.config_option.fstringify_config_option import (
-            FstringifyConfigOption,
-        )
-        from wexample_filestate_python.config_option.modernize_typing_config_option import (
-            ModernizeTypingConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_class_attributes_config_option import (
-            OrderClassAttributesConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_class_docstring_config_option import (
-            OrderClassDocstringConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_class_methods_config_option import (
-            OrderClassMethodsConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_constants_config_option import (
-            OrderConstantsConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_iterable_items_config_option import (
-            OrderIterableItemsConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_main_guard_config_option import (
-            OrderMainGuardConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_module_docstring_config_option import (
-            OrderModuleDocstringConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_module_functions_config_option import (
-            OrderModuleFunctionsConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_module_metadata_config_option import (
-            OrderModuleMetadataConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_spacing_config_option import (
-            OrderSpacingConfigOption,
-        )
-        from wexample_filestate_python.config_option.order_type_checking_block_config_option import (
-            OrderTypeCheckingBlockConfigOption,
-        )
-        from wexample_filestate_python.config_option.relocate_imports_config_option import (
-            RelocateImportsConfigOption,
-        )
-        from wexample_filestate_python.config_option.remove_unused_config_option import (
-            RemoveUnusedConfigOption,
-        )
-        from wexample_filestate_python.config_option.sort_imports_config_option import (
-            SortImportsConfigOption,
-        )
-        from wexample_filestate_python.config_option.unquote_annotations_config_option import (
-            UnquoteAnnotationsConfigOption,
-        )
-
+        global _OPTION_NAME_TO_ATTR
+        if _OPTION_NAME_TO_ATTR is None:
+            _OPTION_NAME_TO_ATTR = _build_option_name_to_attr()
         return {
-            AddFutureAnnotationsConfigOption.get_name(): self.add_future_annotations,
-            AddReturnTypesConfigOption.get_name(): self.add_return_types,
-            FixAttrsConfigOption.get_name(): self.fix_attrs,
-            FixBlankLinesConfigOption.get_name(): self.fix_blank_lines,
-            FormatConfigOption.get_name(): self.format,
-            FstringifyConfigOption.get_name(): self.fstringify,
-            ModernizeTypingConfigOption.get_name(): self.modernize_typing,
-            OrderClassAttributesConfigOption.get_name(): self.order_class_attributes,
-            OrderClassDocstringConfigOption.get_name(): self.order_class_docstring,
-            OrderClassMethodsConfigOption.get_name(): self.order_class_methods,
-            OrderConstantsConfigOption.get_name(): self.order_constants,
-            OrderIterableItemsConfigOption.get_name(): self.order_iterable_items,
-            OrderMainGuardConfigOption.get_name(): self.order_main_guard,
-            OrderModuleDocstringConfigOption.get_name(): self.order_module_docstring,
-            OrderModuleFunctionsConfigOption.get_name(): self.order_module_functions,
-            OrderModuleMetadataConfigOption.get_name(): self.order_module_metadata,
-            OrderSpacingConfigOption.get_name(): self.order_spacing,
-            OrderTypeCheckingBlockConfigOption.get_name(): self.order_type_checking_block,
-            RelocateImportsConfigOption.get_name(): self.relocate_imports,
-            RemoveUnusedConfigOption.get_name(): self.remove_unused,
-            SortImportsConfigOption.get_name(): self.sort_imports,
-            UnquoteAnnotationsConfigOption.get_name(): self.unquote_annotations,
+            name: getattr(self, attr)
+            for name, attr in _OPTION_NAME_TO_ATTR.items()
         }
